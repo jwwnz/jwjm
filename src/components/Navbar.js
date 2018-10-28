e01aadd2a614b4aa581b9158af4a038bbc185c63
jwwnz <jameswon.nz@gmail.com>
2018-10-28 20:08:58 +1300

    Update src/components/Navbar.js
    Fix errors with href links.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="">
         JWJM 2019
       </a>
       <button
@@ -20,17 +20,17 @@ export default function Navbar() {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="">
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="">
               Features
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="#">
+            <a className="nav-link disabled" href="">
               Disabled
             </a>
           </li>
