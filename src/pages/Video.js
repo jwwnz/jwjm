f795db868b06d8179eda45193df353b94c5bb448
jwwnz <jwon117@aucklanduni.ac.nz>
2018-12-22 21:51:05 +0900

    Bug fix issue with video page

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -8,13 +8,17 @@ export default class Video extends Component {
                 <h1>Video</h1>
                 <div className="row">
                     <div className="padding-medium font-message">Prestige Law, James' previous workplace of 5 years sent us a touching video wishing us well for the wedding.</div>
-                    <div className="padding-medium">
-                        <VideoComponent />
+
+                    <div className="row">
+                        <div className="padding-medium">
+                            <VideoComponent />
+                        </div>
                     </div>
 
+
                     <div className="padding-top font-message">Thank you to Royal and everyone at Prestige Law for taking the time to prepare and send this to us!
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
                         To check out Prestige law, <a href="https://prestige.law/">prestige.law/</a>
                     </div>
                 </div>
