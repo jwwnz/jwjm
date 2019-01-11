7bd0dc225ef4858588bd2cd0dec7ba2121e94d8f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-11 15:45:40 +0000

    Deploy jwwnz/jwjm to github.com/jwwnz/jwjm.git:gh-pages

---
+++ precache-manifest.750ad76da6b2982dc805b7787528b232.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebdf7a894ce495d1710c",
+    "url": "/static/css/main.cf80478a.chunk.css"
+  },
+  {
+    "revision": "ebdf7a894ce495d1710c",
+    "url": "/static/js/main.ebdf7a89.chunk.js"
+  },
+  {
+    "revision": "7282f86f1c54338002aa",
+    "url": "/static/js/1.7282f86f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "977379150903e2694e1935681ea6e108",
+    "url": "/static/media/1.97737915.JPG"
+  },
+  {
+    "revision": "b2294893453351fbac25ed2cad5fe4bc",
+    "url": "/static/media/2.b2294893.JPG"
+  },
+  {
+    "revision": "9c9cb5b227be21bc70b97cc8e86ac7bc",
+    "url": "/static/media/3.9c9cb5b2.PNG"
+  },
+  {
+    "revision": "b7b484b20ab12abada41aebe61c96c0b",
+    "url": "/static/media/4.b7b484b2.PNG"
+  },
+  {
+    "revision": "81b9fdfe4fd5a7579b627d16abee0704",
+    "url": "/static/media/5.81b9fdfe.PNG"
+  },
+  {
+    "revision": "9cf4cbc883715893245ce64c82e0f21b",
+    "url": "/static/media/6.9cf4cbc8.PNG"
+  },
+  {
+    "revision": "5df6eb45636f09d9e7455b935cc521d4",
+    "url": "/static/media/7.5df6eb45.JPG"
+  },
+  {
+    "revision": "5c6929dde0bf36df73f6046d7164fff7",
+    "url": "/static/media/8.5c6929dd.JPG"
+  },
+  {
+    "revision": "2937b7bc066c09eae047c83c7658cba0",
+    "url": "/static/media/9.2937b7bc.JPG"
+  },
+  {
+    "revision": "605971e86905e2e2370c5228b43c89cb",
+    "url": "/static/media/10.605971e8.JPG"
+  },
+  {
+    "revision": "779a8dcd4c6d2e634ac09abfc4ade0c2",
+    "url": "/static/media/invitation0.779a8dcd.jpg"
+  },
+  {
+    "revision": "ded554d45af252068d486bba018c5818",
+    "url": "/static/media/invitation1.ded554d4.jpg"
+  },
+  {
+    "revision": "4d7a99923c749a6d2afa8fce50ce80fd",
+    "url": "/static/media/translation.4d7a9992.pdf"
+  },
+  {
+    "revision": "48f7cf1ba5c0acbc66f7f9922feae100",
+    "url": "/index.html"
+  }
+];
